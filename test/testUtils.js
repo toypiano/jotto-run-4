2220b7865c70d747b7cc6f2305350129de9c4432
Hayoun Lee <sidhlee@gmail.com>
2020-01-21 12:58:54 +0900

    Add test util: storeFactory

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,8 @@
 import checkPropTypes from "check-prop-types";
+import { createStore, applyMiddleware } from "redux";
+
+import middlewares from "../src/app/store";
+import rootReducer from "../src/app/rootReducer";
 
 /**
  * Will return error if expectedProps don't match the set propTypes
@@ -28,4 +32,24 @@ export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 };
 
-export const storeFactory = preloadedState => {};
+/* 
+We are testing Guess which is wrapped in Redux.connect() and exported.
+In our actual app, <Guess /> will be within <App /> which is wrapped in <Provider />
+For our tests, we need to create a fresh store that matches configuration for the actual store and pass it as props for each test. Otherwise, you'll get this error in the console:
+
+Invariant Violation: Could not find "store" in the context of "Connect(Guess)". Either wrap the root component in a <Provider>, or pass a custom React context provider to <Provider> and the corresponding React context consumer to Connect(Guess) in connect options.
+*/
+
+/**
+ * Create a testing store with imported reducers, middleware, and preloaded state.
+ * @function storeFactory
+ * @param {Object} preloadedState - preloaded state for store.
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = preloadedState => {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(...middlewares)
+  );
+};
