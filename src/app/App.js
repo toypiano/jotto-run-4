04be8675b509c61d868d783e7607f0a875c708bd
Hayoun Lee <sidhlee@gmail.com>
2020-01-21 09:13:50 +0900

    Mock MVP in App component

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Zotto run4</div>;
+  return (
+    <div className="container d-flex flex-column align-items-center mt-4">
+      <header>
+        <h1>Jotto</h1>
+      </header>
+      <main>
+        <section>Congrats Message</section>
+        <section>
+          guessForm
+          <div>guess input</div>
+          <div>Submit Button</div>
+          <p>Guess the secretWord!</p>
+        </section>
+        <section>
+          guessed words table
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>train</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td>train</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td>train</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
