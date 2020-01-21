8d3b4e539bebd4be00329ef41b0c39c5fbc9054e
Hayoun Lee <sidhlee@gmail.com>
2020-01-21 09:58:26 +0900

    Add test descriptions for Congrats

--- src/features/congrats/Congrats.test.js
+++ src/features/congrats/Congrats.test.js
@@ -1,2 +1,13 @@
 import React from "react";
 import { shallow } from "enzyme";
+
+import { findByTestAttr, checkProps } from "../../../test/testUtils";
+import Congrats from "./Congrats";
+
+describe("render", () => {
+  test("renders without error", () => {});
+  test("renders no text when 'success' prop is false", () => {});
+  test("renders non-empty congrats message when success prop is true", () => {});
+});
+
+test("does not throw warning with expected props", () => {});
