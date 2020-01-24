efa64ce7b7943f9a1d5ed3486fa6747ac2aa42ee
Hayoun Lee <sidhlee@gmail.com>
2020-01-24 13:33:47 +0900

    Add event handlers to Guess component to pass tests

--- src/features/guess/Guess.js
+++ src/features/guess/Guess.js
@@ -6,16 +6,33 @@ class Guess extends Component {
     inputValue: ""
   };
 
+  handleChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.guessWord(this.state.inputValue);
+    this.setState({ inputValue: "" });
+  };
+
   render() {
     const form = this.props.success ? null : (
-      <form className="form-group" data-test="guess-form">
+      <form
+        className="form-group"
+        data-test="guess-form"
+        onSubmit={this.handleSubmit}
+      >
         <label for="word">Guess the secret word!</label>
         <input
           data-test="input-control"
           id="word"
           className="form-control"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
         />
         <button
+          type="submit"
           data-test="submit-button"
           className="btn btn-primary mt-2"
         >
