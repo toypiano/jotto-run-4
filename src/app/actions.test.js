8285a8c8aeff3e2f7485487a2f29fc3f6d42b087
Hayoun Lee <sidhlee@gmail.com>
2020-01-24 18:18:53 +0900

    Add integration tests to fetchSecretWord action creator

--- src/app/actions.test.js
+++ src/app/actions.test.js
@@ -4,9 +4,58 @@ import { storeFactory } from "../../test/testUtils";
 import { fetchSecretWord } from "./actions";
 
 describe("fetchSecretWord thunk action creator", () => {
-  test("update secretWord state with fetched word", () => {});
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  test("update secretWord state with fetched word", () => {
+    const secretWord = "piano";
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord
+      });
+    });
+    return store.dispatch(fetchSecretWord()).then(() => {
+      const newState = store.getState();
+      expect(newState.secretWord).toBe(secretWord);
+    });
+  });
   describe("updates serverError state to 'true'", () => {
-    test("when server returns 4xx status", () => {});
-    test("when server returns 5xx status", () => {});
+    let store;
+    beforeEach(() => {
+      store = storeFactory();
+    });
+    test("when server returns 4xx status", () => {
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 404
+        });
+      });
+
+      return store.dispatch(fetchSecretWord()).then(() => {
+        const newState = store.getState();
+        expect(newState.serverError).toBe(true);
+      });
+    });
+    test("when server returns 5xx status", () => {
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 500
+        });
+      });
+
+      return store.dispatch(fetchSecretWord()).then(() => {
+        const newState = store.getState();
+        expect(newState.serverError).toBe(true);
+      });
+    });
   });
 });
