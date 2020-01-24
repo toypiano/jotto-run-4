ce735479f86ceb84fb0e777eefece8a07ba9e512
Hayoun Lee <sidhlee@gmail.com>
2020-01-24 19:00:33 +0900

    Add test description for App component + fix mapState

---
+++ src/app/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import App from "./App";
+import AppContainer from "./AppContainer";
+
+describe("redux props", () => {
+  test("has access to 'success' state", () => {});
+  test("has access to 'fetchSecretWord' action creator'", () => {});
+});
+
+test("'fetchSecretWord' runs on App mount", () => {});
--- src/app/AppContainer.js
+++ src/app/AppContainer.js
@@ -3,6 +3,6 @@ import { connect } from "react-redux";
 import App from "./App";
 import { fetchSecretWord } from "./actions";
 
-const mapState = ({ secretWord }) => ({ secretWord });
+const mapState = ({ success }) => ({ success });
 
 export default connect(mapState, { fetchSecretWord })(App);
