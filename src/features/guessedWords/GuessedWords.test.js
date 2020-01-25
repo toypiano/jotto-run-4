13a6e0e5fbaa4ed4ed34d4668a19fed8df9292ea
Hayoun Lee <sidhlee@gmail.com>
2020-01-25 13:03:03 +0900

    Add test descriptions for GuessedWords component

--- src/features/guessedWords/GuessedWords.test.js
+++ src/features/guessedWords/GuessedWords.test.js
@@ -2,3 +2,15 @@ import React from "react";
 import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../../../test/testUtils";
 import GuessedWords from "./GuessedWords";
+
+test("does not throw warning with expected props", () => {});
+
+describe("no words guessed previously", () => {
+  test("renders without error", () => {});
+  test("renders an instructions to guess a word", () => {});
+});
+
+describe("some words guessed previously", () => {
+  test("renders without error", () => {});
+  test("renders 'guessedWords' section", () => {});
+});
