f0ae52b203902ceba965aef76ef50789acda4ba6
Hayoun Lee <sidhlee@gmail.com>
2020-01-26 13:23:19 +0900

    Add tests for server.js

--- server/server.test.js
+++ server/server.test.js
@@ -2,6 +2,19 @@ const request = require("supertest");
 const app = require("./server");
 
 describe("root path", () => {
-  test("responds with status 200 on GET", () => {});
-  test("response is a five-letter word", () => {});
+  test("responds with status 200 on GET", () => {
+    // wait for async call
+    return request(app)
+      .get("/")
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
+  test("response is a five-letter word", () => {
+    return request(app)
+      .get("/")
+      .then(res => {
+        expect(res.text.length).toBe(5);
+      });
+  });
 });
