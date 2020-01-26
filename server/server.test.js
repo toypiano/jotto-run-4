9af805da3974bda213a65486cdd48d89ad454ebe
Hayoun Lee <sidhlee@gmail.com>
2020-01-26 13:20:03 +0900

    Add test descriptions for server.js

---
+++ server/server.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest");
+const app = require("./server");
+
+describe("root path", () => {
+  test("responds with status 200 on GET", () => {});
+  test("response is a five-letter word", () => {});
+});
