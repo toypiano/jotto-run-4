997d96b4bc93f807089e8dd53a98a78c3eca9aa5
Hayoun Lee <sidhlee@gmail.com>
2020-01-27 20:43:24 +0900

    Start ServerError component

---
+++ src/features/serverError/ServerError.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ServerError = props => {
+  return <div></div>;
+};
+
+ServerError.propTypes = {};
+
+export default ServerError;
---
+++ src/features/serverError/ServerError.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../../../test/testUtils";
+
+describe("render", () => {
+  test("renders without error", () => {});
+  test("renders non-empty error text", () => {});
+});
