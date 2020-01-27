3b7c4d3d55ad3e5c9b8426624c013bfd6a96d0f7
Hayoun Lee <sidhlee@gmail.com>
2020-01-27 17:05:00 +0900

    Add giveUp action creator to Guess Container

--- src/features/guess/GuessContainer.js
+++ src/features/guess/GuessContainer.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
-import { guessWord } from "./GuessActions";
+import { guessWord, giveUp } from "./GuessActions";
 import Guess from "./Guess";
 
 const mapState = ({ success }) => ({ success });
-const actionCreators = { guessWord };
+const actionCreators = { guessWord, giveUp };
 export default connect(mapState, actionCreators)(Guess);
