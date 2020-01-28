5f7ffc4a43968e2ffb38c86e8e4e6f7c6d2f782f
Hayoun Lee <sidhlee@gmail.com>
2020-01-28 18:30:25 +0900

    Add test desctiptions for EnterWordForm

---
+++ src/features/enterWord/EnterWordForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class EnterWordForm extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+EnterWordForm.propTypes = {};
+export default EnterWordForm;
---
+++ src/features/enterWord/EnterWordForm.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+describe("render", () => {
+  test("renders without error", () => {});
+  test("renders instructions", () => {});
+  test("render submit button", () => {});
+  test("renders input control", () => {});
+});
+
+test("does not throw warning with expected props", () => {});
+
+describe("submitUserWord action", () => {
+  test("'submitUserWord' was called on submit", () => {});
+  test("'submitUserWord' was called with input value as argument", () => {});
+});
