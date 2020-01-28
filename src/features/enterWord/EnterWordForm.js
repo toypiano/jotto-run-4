94d6c593b0bd7193c9a0967f240d6d7ba5f0aaae
Hayoun Lee <sidhlee@gmail.com>
2020-01-28 18:57:19 +0900

    Add tests for EnterWordForm component

--- src/features/enterWord/EnterWordForm.js
+++ src/features/enterWord/EnterWordForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+// Conditionally rendered in App component
+// because it replaces some other components
 class EnterWordForm extends Component {
   render() {
     return <div></div>;
--- src/features/enterWord/EnterWordForm.test.js
+++ src/features/enterWord/EnterWordForm.test.js
@@ -1,16 +1,83 @@
 import React from "react";
 import { shallow } from "enzyme";
 
+import EnterWordForm from "./EnterWordForm";
+import { findByTestAttr, checkProps } from "../../../test/testUtils";
+
+const defaultProps = {
+  submitUserWord: () => {}
+};
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<EnterWordForm {...setupProps} />);
+};
+
 describe("render", () => {
-  test("renders without error", () => {});
-  test("renders instructions", () => {});
-  test("render submit button", () => {});
-  test("renders input control", () => {});
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup();
+  });
+  test("renders without error", () => {
+    const component = findByTestAttr(
+      wrapper,
+      "component-enter-word-form"
+    );
+    expect(component.length).toBe(1);
+  });
+  test("renders instructions", () => {
+    const instructions = findByTestAttr(
+      wrapper,
+      "enter-word-instructions"
+    );
+    expect(instructions.text().length).toBeGreaterThan(0);
+  });
+  test("render submit button", () => {
+    const submitButton = findByTestAttr(
+      wrapper,
+      "word-submit-button"
+    );
+    expect(submitButton.length).toBe(1);
+  });
+  test("renders input control", () => {
+    const inputControl = findByTestAttr(
+      wrapper,
+      "submit-word-input-control"
+    );
+    expect(inputControl.length).toBe(1);
+  });
 });
 
-test("does not throw warning with expected props", () => {});
+test("does not throw warning with expected props", () => {
+  const expectedProps = defaultProps;
+  const error = checkProps(EnterWordForm, expectedProps);
+  expect(error).toBe(undefined);
+});
 
 describe("submitUserWord action", () => {
-  test("'submitUserWord' was called on submit", () => {});
-  test("'submitUserWord' was called with input value as argument", () => {});
+  let wrapper, submitUserWordMock;
+  const userSecretWord = "dance";
+
+  beforeEach(() => {
+    submitUserWordMock = jest.fn();
+    wrapper = shallow(
+      <EnterWordForm submitUserWord={submitUserWordMock} />
+    );
+    // EnterWordForm class component has instance property, 'inputControl'
+    // which is a ref passed to the 'input-control' element.
+    // React assigns 'current' props with the DOM element when
+    // the component mounts, and assigns it back to 'null' when it un-mounts.
+    wrapper.instance().inputControl.current = {
+      value: userSecretWord
+    };
+    const form = findByTestAttr(wrapper, "component-enter-word-form");
+    // provide fake event obj into event handler callback
+    // to prevent ref error when e.preventDefault is called.
+    form.simulate("submit", { preventDefault: () => {} });
+  });
+  test("'submitUserWord' was called on submit", () => {
+    expect(submitUserWordMock.mock.calls.length).toBe(1);
+  });
+  test("'submitUserWord' was called with input value as argument", () => {
+    expect(submitUserWordMock.mock.calls[0][0]).toBe(userSecretWord);
+  });
 });
